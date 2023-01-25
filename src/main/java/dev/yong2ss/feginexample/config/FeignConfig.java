e139694b44170fe9d6c43e11ac43a0e99d2ba879
jeongyolee <jeongyolee@ebay.com>
2023-01-25 19:17:51 +0900

    feat: feignClient CustomLogger 추가

--- src/main/java/dev/yong2ss/feginexample/config/FeignConfig.java
+++ src/main/java/dev/yong2ss/feginexample/config/FeignConfig.java
@@ -1,8 +1,16 @@
 package dev.yong2ss.feginexample.config;
 
+import dev.yong2ss.feginexample.feign.logger.FeignCustomLogger;
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FeignConfig {
+    //모든 FeignClient Global 설정
+    @Bean
+    public Logger feignLogger() {
+        return new FeignCustomLogger();
+    }
 
 }
